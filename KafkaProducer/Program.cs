ad2eecdda519cac7484a072459ef7364f17407b9
Dustin Johnson <djohnson@whitehotlogic.com>
2017-10-27 15:28:49 -0700

    Fix off-by-one bug in ringbuffer

--- KafkaProducer/Program.cs
+++ KafkaProducer/Program.cs
@@ -1,12 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-
-using System;
-using System.Text;
-using System.Collections.Generic;
 using Confluent.Kafka.Serialization;
 using Confluent.Kafka;
 using System.IO;
--- UniversalStreamReader/Program.cs
+++ UniversalStreamReader/Program.cs
@@ -1,19 +1,12 @@
 ﻿using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Collections.Specialized;
 using System.IO;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
-
-// for the ring buffer -- am i doing this wrong by using threads? do i even fully understand a ring buffer? look into it further.
 using System.Threading.Tasks;
 using System.Linq;
 
 // for the kafka consumer (using confluent c# wrapper for librdkafka C client)
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using Confluent.Kafka.Serialization;
 using Confluent.Kafka;
@@ -23,20 +16,23 @@ namespace UniversalStreamReader
     class Program
     {
 
+        // todo: is the cache appending to itself? because it shouldn't
+        // cache should read from disk, then wipe the file?
+
+
         static void Main(string[] args)
         {
 
             const string DATABASE_FILE = @"c:\tempDev\AllData.sqlite";
-            const string BOOTSTRAP_SERVERS = "192.168.1.118:9092";
+            const string STREAM_SERVERS = "192.168.1.118:9092";
             List<string> topicsToConsume = new List<string>();
             topicsToConsume.Add("testaroo");
             //topicsToConsume.Add("dummytopic1");
             //topicsToConsume.Add("dummytopic5");
             //topicsToConsume.Add("dummytopic6");
 
-
             //create background thread to consume kafka cluster
-            Task.Factory.StartNew(() => StreamConsumer_Kafka.Run_Poll(BOOTSTRAP_SERVERS, topicsToConsume));
+            Task.Factory.StartNew(() => StreamConsumer_Kafka.Run_Poll(STREAM_SERVERS, topicsToConsume));
            
             while (true)
             {
@@ -44,29 +40,6 @@ namespace UniversalStreamReader
                 Task.Delay(TimeSpan.FromMilliseconds(1000)).Wait();
             }
 
-
-            /*
-            // add some dummy topics and messages to the in-memorycache for testing concurrency
-            int i = 0;
-            while (true)
-            {
-
-                string topic = "dummytopic" + i; // create hash for topic
-                for (int j = 0; j < 20; j++) { // add 20 dummy messages to each topic
-                    string message = "dummymessage" + j;
-                    imcwp.add(topic, message, DateTime.Now);
-                }
-                
-                //Task.Delay(TimeSpan.FromSeconds(0.1)).Wait();
-
-                //on close, persist to disk
-                imcwp.persist();
-
-                i++;
-            }
-            */
-
-            Task.Delay(TimeSpan.FromSeconds(5)).Wait(); // so i can see the output for now
 
         }
     }
@@ -91,7 +64,7 @@ namespace UniversalStreamReader
     class StreamConsumer_Kafka {
 
         const string PERSISTENCE_FILE = @"c:\tempDev\CacheData.txt";
-        const int RINGBUFFER_SIZE = 10;
+        const int RINGBUFFER_SIZE = 5;
 
         private static Dictionary<string, object> constructConfig(string brokerList, bool enableAutoCommit) =>
                                                                                 new Dictionary<string, object>
@@ -131,8 +104,6 @@ namespace UniversalStreamReader
                     //write to in-memory cache
                     imcwfp.addMessage(msg.Topic, msg.Value, DateTime.UtcNow);
 
-                    //write to DB
-
                 };
 
                 consumer.OnPartitionEOF += (_, end)
@@ -169,13 +140,12 @@ namespace UniversalStreamReader
 
                 // output periodic statistics for debug purposes
                 //consumer.OnStatistics += (_, json)
-                //    => Console.WriteLine($"Statistics: {json}");
+                //    => Console.WriteLine($"DEBUG: Statistics: {json}");
 
                 consumer.Subscribe(topics);
 
                 Console.WriteLine($"Subscribed to: [{string.Join(", ", consumer.Subscription)}]");
 
-               
 
                 var cancelled = false;
                 Console.CancelKeyPress += (_, e) => {
@@ -186,15 +156,9 @@ namespace UniversalStreamReader
                 Console.WriteLine("Ctrl-C to exit.");
                 while (!cancelled)
                 {
-                    //Console.WriteLine("polling");
+                    //Console.WriteLine("DEBUG: polling");
                     consumer.Poll(TimeSpan.FromMilliseconds(100));
-                    
-                    
-                    /* Java way:
-                     * for (ConsumerRecord<String, String> record : records) // get all the messages and log them to console
-                     * System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
-                     * 
-                     * */
+
                 }
             }
         }
@@ -216,15 +180,7 @@ namespace UniversalStreamReader
     class InMemoryCacheWithFilePersist
     {
 
-
-
-
-
-        //ConcurrentDictionary<string,List<KafkaMessage>> cache = null;
-        //ConcurrentDictionary<string,Dictionary<DateTime,string>> cache = null; // use ConcurrentDictionary because it's thread-safe with atomic ops, and O(1)
-        //SortedDictionary<string,List<string,string>> cache = null;
         string[,] stringArrayCache = null;
-
 
         private String persistenceFilePath = null;
 
@@ -235,52 +191,51 @@ namespace UniversalStreamReader
         public InMemoryCacheWithFilePersist(int ringBufferSize, string persistenceFilePath)
         {
 
-            //this.cache = new ConcurrentDictionary<string,Dictionary<DateTime,string>>(); //for initial testing, use strings for key/value
-            //this.cache = new ConcurrentDictionary<string,List<KafkaMessage>>();
-            //this.cache = new SortedDictionary<string,string>();
-
             this.ringBufferSize = ringBufferSize;
             stringArrayCache = new string[ringBufferSize, 3]; // number of rows in array predefined by ringBufferSize
                                                               // three strings are created, topic, message
             
 
-            this.persistenceFilePath = persistenceFilePath;
+            this.persistenceFilePath = persistenceFilePath; // we need the value for other methods
 
 
             try // check and see if cache persistence file exists, and if not, create it
             {
                 if (!File.Exists(persistenceFilePath))
                 {
-                    File.Create(persistenceFilePath).Dispose();
+                    File.Create(persistenceFilePath).Dispose(); // create the cache persist file on disk
                 }
             } catch (IOException e)
             {
                 Console.WriteLine("ERROR: Could not create cache persistence file -- " + e.Message);
             }
 
-            try // will get FileNotFoundException if no cache available on disk
+            try // try to load the persistent cache file into memory
             {
                 using (FileStream fileStream = new FileStream(persistenceFilePath, FileMode.Open))
                 {
-
-                    //assumes cache size in file matches cache size specified in RINGBUFFER_SIZE
-
-
-                    //this.cache = (ConcurrentDictionary<string,List<KafkaMessage>>)bf.Deserialize(fileStream);
-                    //this.cache = (ConcurrentDictionary<string,Dictionary<DateTime,string>>)bf.Deserialize(fileStream);
-                    //this.cache = (OrderedDictionary)bf.Deserialize(fileStream); 
-                    //this.cache = (SortedDictionary<DateTime,string>) bf.Deserialize(fileStream);
-
-
                     IFormatter bf = new BinaryFormatter();
-                    this.stringArrayCache = (string[,]) bf.Deserialize(fileStream); 
+                    this.stringArrayCache = (string[,]) bf.Deserialize(fileStream);  //assumes cache size in file matches cache 
+                                                                                     //  size specified in RINGBUFFER_SIZE
+
                     fileStream.Close();
-
-                    // sort by DateTime.UtcNow, so the oldest cache entry is at the beginning, and GO
-                    this.stringArrayCache = stringArrayCache.OrderBy(row => row[0]); // row[0] is the created (datetime.utcnow)
-
-
                 }
+
+                using (var fs = new FileStream(persistenceFilePath, FileMode.Truncate)) { } // wipe the disk file
+
+
+                // sort by DateTime.UtcNow, so the oldest cache entry is at the beginning, and GO
+                this.stringArrayCache = stringArrayCache.OrderBy(row => row[0]); // row[0] is the datetime.now.ticks when it was added
+
+                // rewrite the sorted array to file, so it matches the cache
+                using (FileStream fileStream = new FileStream(persistenceFilePath, FileMode.Create))
+                {
+                    IFormatter bf = new BinaryFormatter();
+                    bf.Serialize(fileStream, this.stringArrayCache);
+                    fileStream.Close();
+                }
+
+
             }
             catch (FileNotFoundException e)
             {
@@ -290,10 +245,8 @@ namespace UniversalStreamReader
             {
                 // if empty, just display warning
                 // if corrupt, display ERROR and quit program waiting 10secs for user to view error
-                Console.WriteLine("WARNING: Persistence File Empty or Corrupt  -- " + e.Message);
+                Console.WriteLine("WARNING: Persistence file is empty or unexpected format  -- " + e.Message);
             }
-            
-
             
 
             Console.WriteLine("INFO: In-Memory Cache Initialized with " + ringBufferSize + " records");
@@ -303,7 +256,7 @@ namespace UniversalStreamReader
         public void addMessage(string topic, string message, DateTime created)
         {
 
-            if (indexToWrite == ringBufferSize - 1) // if we've reached the end of the ringbuffer / cache
+            if (indexToWrite > ringBufferSize - 1) // if we've reached the end of the ringbuffer / cache
                 indexToWrite = 0;
 
             this.stringArrayCache[indexToWrite, 0] = created.Ticks.ToString(); // gives me a sortable  column
@@ -312,14 +265,18 @@ namespace UniversalStreamReader
 
             indexToWrite++; // set pointer to next index in the cache for next message write (seems ringbuffery to me)
 
-            Console.WriteLine("INFO: topic: \"" + topic + "\", with message: \"" + message + "\" has been added to cache at \"" + created.ToString() + "\"");
+            Console.WriteLine("INFO: topic: \"" + topic + "\", with message: \"" + message + "\" has been added to cache at \"" + created.Ticks.ToString() + "\"");
 
+            //write to persisted cache disk file
             serializedFilePersist();
-
             Console.WriteLine("INFO: Cache persisted to disk file");
 
-            
+            //write to DB
+            sqliteDatabasePersist();
+            Console.WriteLine("INFO: Message added to sqlite database");
+
         }
+
 
         public void serializedFilePersist()
         {
@@ -329,13 +286,13 @@ namespace UniversalStreamReader
                 bf.Serialize(fileStream, this.stringArrayCache);
                 fileStream.Close();
             }
-
         }
+
 
         public void sqliteDatabasePersist()
         {
 
-            // todo: use ORM wrapper like Dapper for query sanitization + better readability
+            // would be nice: use ORM wrapper like Dapper for query sanitization + better readability
 
             // pseudocoded for the assumptions:
             //   1) that the database should update one message at a time, to stay in sync with the cache
@@ -353,80 +310,14 @@ namespace UniversalStreamReader
                     // else, check each message for existence in the db and insert if it doesn't
 
         }
-
-        /*
-        public int size()
-        {
-            //return this.cache.Count; // needed for ringbuffer byCount option? nope, this is wrong ---->
-                                              // ----> need to ringbuffer by message count per topic, not topic (key) count 
-        }
-        */
-
-        /*
-        public Dictionary<DateTime, string> getMessagesFromCacheForTopic(string topic)
-        {
-            //if (this.cache.ContainsKey(topic))
-            if (this.cache.Contains(topic))
-            {
-                return cache[topic] as Dictionary<DateTime, string>; // return all messages for this topic
-            } else
-            {
-                return new Dictionary<DateTime, string>(); // returning empty for testing only
-            }
-            
-        }
-        */
-
-        /*
-        public void ringBufferPolicer (int policyType, int expiryValue)
-        {
-            //policyType==count, expiryValue==3      (remove oldest message if messagecount is > 100) 
-            //policyType==time, expiryValue==600_000   (10 minutes in milliseconds)
-
-            while (true)
-            {
-
-                //Console.WriteLine("Loop of Background Thread");
-                //for(each topic in cache, get messages count)
-                foreach(KeyValuePair<string,Dictionary<DateTime,string>> kvp in this.cache)
-                {
-                    
-                    string topic = kvp.Key;
-
-                    //Console.WriteLine(topic);
-                    //Task.Delay(TimeSpan.FromSeconds(1)).Wait();
-
-                    Dictionary<DateTime, string> messages = kvp.Value; // just for ease of readability
-
-                    if (messages.Count > expiryValue) // then expire the messages, (assuming count for current testing purposes)
-                    {
-
-                        int expiredCount = messages.Count - expiryValue; // get the number of messages to remove
-
-                        //sort the dictionary, take the top X values
-                        messages = messages.OrderBy(pair => pair.Key).Take(expiryValue)
-                                                                     .ToDictionary(pair => pair.Key, pair => pair.Value);
-
-                        //remove the expired values by reassigning the trimmed dictionary to the cache 
-                        this.cache[topic] = messages;
-
-                        Console.WriteLine("INFO: RingbuffPolicer found and removed " + expiredCount + " expired messages for topic: " + topic);
-
-                    }
-
-                }
-            }
-     
-
-
-        }
-        */
-
-
-
-
-
+        
     }
+
+
+    //
+    // below is just a class i found for sorting multidimensional arrays by column, which i need for when i restore
+    //    the persisted disk cache back into memory
+    // 
 
     public static class MultiDimensionalArrayExtensions
     {
@@ -497,29 +388,5 @@ namespace UniversalStreamReader
         }
     }
 
-
-    /**
-    * 
-    * 
-    * 
-    *    Topic, Message Objects, for ORM
-    * 
-    * 
-    * 
-    * 
-    */
-
-    class KafkaMessage
-    {
-        public int Id { get; set; }
-        public string Value { get; set; }
-        public DateTime Created { get; set; }
-    }
-
-    class KafkaTopic
-    {
-        public int Id { get; set; }
-        public string Value { get; set; }
-    }
 
 }
